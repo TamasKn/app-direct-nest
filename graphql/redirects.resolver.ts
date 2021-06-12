ac050a96906300fbb95b5676c5873bf9e44c7b4a
TamasKn <tamas.knisz@gmail.com>
2021-06-12 20:19:01 +0200

    Database operation added to Redirects Service

--- graphql/redirects.resolver.ts
+++ graphql/redirects.resolver.ts
@@ -1,13 +1,14 @@
-import {Resolver, Query} from '@nestjs/graphql';
+import {Resolver, Query, Mutation, Args} from '@nestjs/graphql';
 import {RedirectsType} from './redirects.type';
 import {RedirectsService} from '../src/redirects/redirects.service';
+import {Headers} from '../src/redirects/header.decorator';
 
 @Resolver((of) => RedirectsType)
 export class RedirectsResolver {
   constructor(private redirectsService: RedirectsService) {}
 
-  @Query((returns) => [RedirectsType])
-  redirects() {
-    return this.redirectsService.redirects();
+  @Mutation((returns) => RedirectsType)
+  redirect(@Args('app') app: string, @Headers() headers: any) {
+    return this.redirectsService.redirect(app, headers);
   }
 }
--- src/redirects/redirects.service.ts
+++ src/redirects/redirects.service.ts
@@ -1,13 +1,57 @@
 import {Injectable} from '@nestjs/common';
 import {PrismaService} from '../../prisma/prisma.service';
-import {redirects as Redirects} from '@prisma/client';
+import {redirects as Redirects, Prisma} from '@prisma/client';
 
 @Injectable()
 export class RedirectsService {
+  private apps = {
+    jifflr: {
+      web: 'https://jifflr.com/',
+      ios: 'https://apps.apple.com/gb/app/jifflr/id1434427409',
+      android:
+        'https://play.google.com/store/apps/details?id=uk.jifflr.app&hl=en_GB',
+    },
+  };
+
   constructor(private db: PrismaService) {}
 
-  // Get all apps and its redirects data from DB
-  async redirects(): Promise<Redirects[]> {
-    return await this.db.redirects.findMany();
+  // Retrieve the appropriate URL if the app is exist in our record
+  async redirect(app, headers): Promise<void> {
+    const name = app.toLowerCase();
+    const device = this.deviceParser(headers['user-agent']);
+
+    if (this.apps[name]) {
+      await this.db.redirects.update({
+        where: {
+          id: 6,
+        },
+        data: {
+          [device]: {increment: 1},
+        },
+      });
+
+      // Redirect to the appropriate page
+
+      // Make this function with REST API because of redirect and url parsing
+    } else {
+      console.log('No app');
+    }
+  }
+
+  // Parsing the user's device OS
+  deviceParser(agent: string): string {
+    let device;
+
+    if (agent.includes('Android')) {
+      device = 'android';
+    } else if (agent.includes('iPhone')) {
+      device = 'ios';
+    } else if (agent.includes('iPad')) {
+      device = 'ios';
+    } else {
+      device = 'web';
+    }
+
+    return device;
   }
 }
