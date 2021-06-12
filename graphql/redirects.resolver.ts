149357636d9d3a2ce683a7ba4886dde3aa223c6b
TamasKn <tamas.knisz@gmail.com>
2021-06-13 01:17:49 +0200

    GraphQL query for every redirects in the database

--- graphql/redirects.resolver.ts
+++ graphql/redirects.resolver.ts
@@ -1,14 +1,13 @@
-import {Resolver, Query, Mutation, Args} from '@nestjs/graphql';
+import {Resolver, Query, Args} from '@nestjs/graphql';
 import {RedirectsType} from './redirects.type';
 import {RedirectsService} from '../src/redirects/redirects.service';
-import {Headers} from '../src/redirects/header.decorator';
 
 @Resolver((of) => RedirectsType)
 export class RedirectsResolver {
   constructor(private redirectsService: RedirectsService) {}
 
-  @Mutation((returns) => RedirectsType)
-  redirect(@Args('app') app: string, @Headers() headers: any) {
-    return this.redirectsService.redirect(app, headers);
+  @Query((returns) => [RedirectsType])
+  redirects() {
+    return this.redirectsService.redirects();
   }
 }
