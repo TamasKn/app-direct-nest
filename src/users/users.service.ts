90b21728bfe0c768ceb28fa3f10940982dc3eee7
TamasKn <tamas.knisz@gmail.com>
2021-06-14 16:57:41 +0200

    User service method updated according to Login operation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,30 @@
-import {Injectable} from '@nestjs/common';
-import {users as Users} from '@prisma/client';
+import {Injectable, InternalServerErrorException} from '@nestjs/common';
 import {PrismaService} from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+export interface LoginResponse {
+  success: boolean;
+}
 
 @Injectable()
 export class UsersService {
   constructor(private db: PrismaService) {}
 
   // Get all users from Database
-  async users(): Promise<Users[]> {
-    return await this.db.users.findMany();
+  async user(name, password): Promise<LoginResponse> {
+    try {
+      const user = await this.db.users.findUnique({
+        where: {name},
+      });
+
+      // If login credentials are correct, sends success:true
+      return user && (await bcrypt.compare(password, user.password))
+        ? {success: true}
+        : {success: false};
+    } catch (err) {
+      throw new InternalServerErrorException({
+        message: 'Database not available',
+      });
+    }
   }
 }
