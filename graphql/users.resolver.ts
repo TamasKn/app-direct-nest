f0b4f431302de9c25af5dff6ee2c01f3e9e899f5
TamasKn <tamas.knisz@gmail.com>
2021-06-14 17:03:30 +0200

    Login query added

--- graphql/users.resolver.ts
+++ graphql/users.resolver.ts
@@ -1,13 +1,13 @@
-import {Resolver, Query} from '@nestjs/graphql';
-import {UsersType} from './users.type';
+import {Resolver, Query, Args} from '@nestjs/graphql';
+import {ResponseType} from './users.type';
 import {UsersService} from '../src/users/users.service';
 
-@Resolver((of) => UsersType)
+@Resolver((of) => ResponseType)
 export class UsersResolver {
   constructor(private usersService: UsersService) {}
 
-  @Query((returns) => [UsersType])
-  users() {
-    return this.usersService.users();
+  @Query((returns) => ResponseType)
+  login(@Args('name') name: string, @Args('password') password: string) {
+    return this.usersService.user(name, password);
   }
 }
